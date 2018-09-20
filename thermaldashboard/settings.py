7d24aa4f88363512d6ca01a17f667423dcb63e05
Zachary Neubert <zacneubert@gmail.com>
2018-09-20 16:02:37 -0600

    Fixed Gamio

--- thermaldashboard/settings.py
+++ thermaldashboard/settings.py
@@ -36,8 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-		'axes.apps.AppConfig',
-		'captcha',
+    'axes.apps.AppConfig',
+    'captcha',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -138,6 +138,17 @@ LOGGING = {
 		},
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    },
+    'axes_cache': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
+
+AXES_CACHE='axes_cache'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
@@ -158,3 +169,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+
